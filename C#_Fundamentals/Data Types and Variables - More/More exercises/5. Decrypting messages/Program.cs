95ec3c0f8840f7a876e28dff797586b179dad4c0
dnedkoff <d.nedkoff@gmail.com>
2022-11-09 21:45:17 +0000

    Update
    More exercises updated

---
+++ C#_Fundamentals/Data Types and Variables - More/More exercises/5. Decrypting messages/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _5._Decrypting_messages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int key = int.Parse(Console.ReadLine());
+            int numberOfLines = int.Parse(Console.ReadLine());
+
+            string word = "";
+
+            for (int i = 0; i < numberOfLines; i++)
+            {
+                char input = char.Parse(Console.ReadLine());
+                int inputToInt = (int)input + key;
+                char word1 = (char)inputToInt;
+                word += word1;
+            }
+            Console.WriteLine(word);
+        }
+    }
+}
---
+++ C#_Fundamentals/Data Types and Variables - More/More exercises/Data Types and Variables - More/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Data_Types_and_Variables___More
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            string dataType; 
+
+            while (input != "END")
+            {
+                if (Int32.TryParse(input, out _))
+                {
+                    dataType = "integer";
+                }
+                else if (double.TryParse(input, out _))
+                {
+                    dataType = "floating point";
+                }
+                else if (bool.TryParse(input, out _))
+                {
+                    dataType = "boolean";
+                }
+                else if (char.TryParse(input, out _))
+                {
+                    dataType = "character";
+                }
+                else // since you cannot parse to string ... if the previous statements came up false -> IT's STRING Type.
+                {
+                    dataType = "string";
+                    
+                }
+                Console.WriteLine($"{input} is {dataType} type");
+                input = Console.ReadLine();
+
+            }
+        }
+    }
+}
